74f22a48bf228c8a4d72a6b0406fe7dff129007e
OrdonezCarol <carol.ordonez@upch.pe>
2025-09-28 16:22:49 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ respuestas["sintomas"] = respuestas_sintomas
 
 # --- Sección F ---
 respuestas["seguro"] = st.radio("¿Cuenta con seguro médico de salud", ["Sí", "No"])
-if respuestas["seguro"] == "Sí":
+if respuestas["seguro"] !== "Sí":
 respuestas["tipo_seguro"] = st.multiselect("¿Qué tipo de seguro medico de salud?", ["Essalud", "SIS", "Privado", "Seguro universitario", "Otro"])
     respuestas["economico"] = st.radio("¿Usted ha dejado de acudir a consultas médicas por motivos económicos", ["Sí", "No"])
 respuestas["distancia"] = st.radio("¿Usted encuentra dificultades para acceder a servicios de salud debido a la distancia", ["Sí", "No"])
