eea6d66bfeacf2b54ad4b7b1467c1218c009d93f
OrdonezCarol <carol.ordonez@upch.pe>
2025-09-28 21:52:31 -0500

    encuesta_salud_radon.py

--- encuesta_salud_radon.py
+++ encuesta_salud_radon.py
@@ -53,23 +53,23 @@ respuestas = {"consentimiento": consentimiento}
 st.markdown('<div class="section-title">Sección A: Datos generales del participante</div>', unsafe_allow_html=True)
 respuestas["edad"] = st.number_input("Edad (años)", min_value=0, step=1)
 respuestas["sexo"] = st.radio("Sexo", ["Femenino", "Masculino"])
-respuestas["nivel_educativo"] = st.selectbox("Nivel educativo", ["Universitario en curso", "Titulado universitario", "Posgrado", "Otro"])
-respuestas["ocupacion"] = st.selectbox("Ocupacion", ["Trabajador dependiente", "Trabajador independiente", "Desempleado", "Ama de casa", "Otro"])
-respuestas["estado_civil"] = st.selectbox("Estado civil", ["Soltero", "Casado", "Viudo", "Divorciado", "Pareja conviviente"])
-respuestas["distrito de residencia"] = st.text_input("Escriba el distrito")
+respuestas["nivel_educativo actual"] = st.selectbox("Nivel educativo", ["Universitario en curso", "Titulado universitario", "Posgrado", "Técnico", "Otro"])
+respuestas["ocupacion actual"] = st.selectbox("Ocupacion", ["Trabajador dependiente", "Trabajador independiente", "Desempleado", "Ama de casa", "Otro"])
+respuestas["estado_civil actual"] = st.selectbox("Estado civil", ["Soltero", "Casado", "Viudo", "Divorciado", "Pareja conviviente"])
+respuestas["distrito de residencia actual"] = st.text_input("Escriba el distrito")
 
 # --- Sección B ---
 st.markdown('<div class="section-title">Sección B: Vinculo con el monitoreo de radon</div>', unsafe_allow_html=True)
 vivienda_actual = st.radio("¿Actualmente vive en la misma vivienda desde el 2016?", ["Sí", "No"])
 respuestas["vive_misma_vivienda"] = vivienda_actual
 if vivienda_actual == "No":
-    respuestas["tiempo_vivio_2023_meses"] = st.number_input("¿Durante cuánto tiempo vivió en esa vivienda en 2016? (meses)", min_value=0, step=1)
+    respuestas["tiempo_vivio_2016_meses"] = st.number_input("¿Durante cuánto tiempo vivió en esa vivienda en 2016? (meses)", min_value=0, step=1)
 respuestas["enf_respiratoria"] = st.multiselect("¿ Dónde colocó el dispositivo de monitoreo de radón?", ["Sala", "Cocina", "Dormitorio", "Baño", "Garaje", "Sotano", "Semisotano", "Primer piso","Segundo piso", "Otro"])
 
 # --- Sección C ---
 st.markdown('<div class="section-title">Sección C: Exposicion ambiental y laboral</div>', unsafe_allow_html=True)
-respuestas["fumaba_2023"] = st.radio("¿Usted fuma?", ["Nunca fumé", "Exfumador", "Fumador actual"])
-if respuestas["fumaba_2023"] in ["Exfumador", "Fumador actual"]:
+respuestas["fumaba_2016"] = st.radio("¿Usted fuma?", ["Nunca fumé", "Exfumador", "Fumador actual"])
+if respuestas["fumaba_2016"] in ["Exfumador", "Fumador actual"]:
     respuestas["paquetes_año"] = st.number_input("Paquetes-año aproximados", min_value=0.0, step=0.1)
 
 respuestas["fumadores_casa"] = st.radio("¿Vive con alguien que fumaba?", ["Sí", "No", "No recuerdo"])
@@ -81,7 +81,7 @@ if respuestas["combustibles"] != "No":
     respuestas["tipo_combustible"] = st.multiselect("¿Qué combustible?", ["Leña", "Carbón", "Kerosene", "Gas", "Otro"])
 
 respuestas["trabajo_expuesto"] = st.radio("¿Ha estado expuesto regularmente al humo del tabaco en su lugar de trabajo?", ["Sí", "No", "No recuerdo"], key="trabajo_expuesto_tabaco")
-respuestas["trabajo_expuesto_carcinogenos"] = st.radio("¿TRabajó en ocupaciones con posibles agentes carcinogenos respiratorios como el asbesto?", ["Sí", "No", "No recuerdo"], key="trabajo_expuesto_carcinogenos")
+respuestas["trabajo_expuesto_carcinogenos"] = st.radio("¿Trabajó en ocupaciones con posibles agentes carcinogenos respiratorios como el asbesto?", ["Sí", "No", "No recuerdo"], key="trabajo_expuesto_carcinogenos")
 if respuestas["trabajo_expuesto_carcinogenos"] == "Sí":
     respuestas["ocupacion_carcinogenos"] = st.text_input("Especifique la ocupación (mineria, soldadura, construccion, industria quimica, etc")
 
@@ -115,11 +115,11 @@ st.markdown('<div class="section-title">Sección F: Identificación de barreras 
 respuestas["seguro"] = st.radio("¿Cuenta con seguro médico de salud", ["Sí", "No"])
 if respuestas["seguro"] == "Sí":
     respuestas["tipo_seguro"] = st.multiselect("¿ Qué tipo de seguro medico de salud?", ["Essalud", "SIS", "Privado", "Seguro universitario", "Otro"])
-respuestas["economico"] = st.radio("¿Usted ha dejado de acudir a consultas médicas por motivos económicos", ["Sí", "No"])
-respuestas["distancia"] = st.radio("¿Usted encuentra dificultades para acceder a servicios de salud debido a la distancia", ["Sí", "No"])
-respuestas["sinatenccion"] = st.radio("¿En algún centro de salud lo han dejado de atender por su condición de étnica, orientación sexual o discapacidad", ["Sí", "No"])
-respuestas["creencias"] = st.radio("¿Ha dejado de realizarse procedimientos médicos por sus creencias religiosas o culturales", ["Sí", "No"])
-respuestas["discriminacion"] = st.radio("¿Ha experimentado discriminación o falta de comprensión cultural por parte del personal médico en el centro de salud que se atiende", ["Sí", "No"])
+respuestas["economico"] = st.radio("¿Usted ha dejado de acudir a consultas médicas por motivos económicos?", ["Sí", "No"])
+respuestas["distancia"] = st.radio("¿Usted encuentra dificultades para acceder a servicios de salud debido a la distancia?", ["Sí", "No"])
+respuestas["sinatenccion"] = st.radio("¿En algún centro de salud lo han dejado de atender por su condición de étnica, orientación sexual o discapacidad?", ["Sí", "No"])
+respuestas["creencias"] = st.radio("¿Ha dejado de realizarse procedimientos médicos por sus creencias religiosas o culturales?", ["Sí", "No"])
+respuestas["discriminacion"] = st.radio("¿Ha experimentado discriminación o falta de comprensión cultural por parte del personal médico en el centro de salud que se atiende?", ["Sí", "No"])
 
 # --- Botón Finalizar ---
 if st.button("Enviar respuestas"):
